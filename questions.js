8449dfc60d990fcce3a6aacd7028a8439981ae63
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-09-07 15:32:03 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"It is a society that is .......... to waging war on all forms of environmental pollution.",
+        options:["committed", "upheld", "referred", "preferred", "regarded"],
+        answer: 0
+    },
+    {
+        q:"Many of the pictures sent from outer space are presently on .......... in the public library.",
+        options:["duty", "display", "account", "exchange", "reinforcement"],
+        answer: 1
+    },
+    {
+        q:"Organizations are often said to be theatres in which people .......... roles in dramas, and psychic prisons, or political arenas.",
+        options:["reinforce", "allege", "perform", "presume", "betray"],
+        answer: 2
+    },
+    {
+        q:"The objective of this project is to increase the depth of awareness and understanding in the world about the causes and consequences of the HIV epidemic through .......... policies and programmes.",
+        options:["poignant", "ambiguous", "meagre", "innovative", "decadent"],
+        answer: 3
+    },
+    {
+        q:"Launched as the new regulatory authority for the country’s banking and financial sector, the National Financial Services Management will extend its activities so as .......... to cover more than 40% of the national economy.",
+        options:["apparently", "fluently", "inherently", "exceptionally", "ultimately"],
+        answer: 4
+    },
+    {
+        q:"To the .......... of those who believe that the new policy will help for the company to get over the long lasting recession, it proved to be far worse than the previous one.",
+        options:["despair", "sanction", "respect", "optimism", "competition"],
+        answer: 0
+    },
+    {
+        q:"In what is viewed as a major diplomatic .......... by Türkiye, Syria agreed to Turkish demands that it abandon its support for Kurdish Workers' Party (PKK) guerrillas.",
+        options:["pretension", "victory", "failure", "execution", "distinction"],
+        answer: 1
+    },
+    {
+        q:"Governments throughout the world .......... that the welfare of their people depends on the economic strength and wealth of the community.",
+        options:["accomplish", "castigate", "assume", "commence", "enlighten"],
+        answer: 2
+    },
+    {
+        q:"Many early peoples who had not yet learned to catch fish, hunt game, or till the soil .......... lived chiefly on roots, berries and nuts.",
+        options:["randomly", "exceedingly", "supremely", "undoubtedly", "inconsistently"],
+        answer: 3
+    },
+    {
+        q:"He devised an .......... scheme whereby the rate of unemployment in the country could be brought down sharply.",
+        options:["illegible", "unfamed", "unstable", "irrelevant", "ingenious"],
+        answer: 4
+    }
+
+
+
+]
